e544dd8a5d60e55f3922d1cadb6993b2732fec87
Ludwig Hedeby <ludwig@hedeby.me>
2018-09-11 10:44:29 +0200

    Add email functionallity

--- src/main/java/com/example/mail/Reminder.java
+++ src/main/java/com/example/mail/Reminder.java
@@ -1,9 +1,15 @@
 package com.example.mail;
 
 import com.example.mail.objects.DBdata;
+import com.example.mail.objects.RealTimeBusesAndMetros;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import java.sql.SQLException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Reminder implements Runnable {
 
@@ -21,11 +27,17 @@ public class Reminder implements Runnable {
 
         MailService emailService = new MailService(javaMailSender);
 
+        if(type.equals("METRO")) {
+            type = "tunnelbana";
+        } else if(type.equals("BUS"))  {
+            type = "buss";
+        }
+
         String to = mail;
-        String subject = "Beräknad ankomsttid är " + time + " till " + origin;
+        String subject = "Din " + type + " förväntas anlända om " + time + " minuter";
         String text = "Hej! " + "\n" +
-                "Beräknad ankomsttid för din " + type +
-                "är " + time + "till hållplats " + origin + "." + "\n" + "\n" +
+                "Din " + type + " förväntas anlända om " + time + " minuter" +
+                " till hållplats " + origin + "." + "\n" + "\n" +
                 "Med vänliga hälsningar" + "\n" +
                 "Push SL";
 
@@ -41,8 +53,28 @@ public class Reminder implements Runnable {
             try {
                 var dbList = repository.listDBdata();
                 for(DBdata data : dbList) {
-                    System.out.println(data);
+                    var realTimeList = apiData.getRealTimeInfo(data.siteInfo, "60");
+                    for(RealTimeBusesAndMetros rt : realTimeList) {
+                        if(rt.JourneyNumber.equals(data.journeynumber)) {
 
+                            rt.ExpectedDateTime = rt.ExpectedDateTime.replace("T", " ");
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                            LocalDateTime realTime = LocalDateTime.parse(rt.ExpectedDateTime, formatter);
+                            LocalDateTime currentTime = LocalDateTime.now();
+                            Duration timeLeft = Duration.between(currentTime, realTime);
+
+                            System.out.println("seconds left: " + timeLeft.getSeconds());
+                            System.out.println(Integer.parseInt(data.timebeforeleavning));
+                            if(timeLeft.getSeconds() / 60 < Integer.parseInt(data.timebeforeleavning)) {
+
+                                sendMail(data.mail, rt.StopAreaName, (timeLeft.getSeconds() / 60) + "", rt.TransportMode);
+                                System.out.println("send mail...");
+                                repository.deleteData(data.journeynumber);
+                                System.out.println("remove from database");
+                            }
+
+                        }
+                    }
                 }
 
             } catch (SQLException e) {
@@ -50,9 +82,8 @@ public class Reminder implements Runnable {
             }
 
 
-//            sendMail("ludwig@hedeby.me", "asdfas", "sdf", "jkshdf");
             try {
-                Thread.sleep(5000);
+                Thread.sleep(30000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
